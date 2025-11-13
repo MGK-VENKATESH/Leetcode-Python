6f4f12561601dbf6d3c439c11315e8c64ed88883
MGK venkatesh <139879845+MGK-VENKATESH@users.noreply.github.com>
2025-11-13 14:18:18 +0530

    Create 387. First Unique Character in a String.py

---
+++ Easy/387. First Unique Character in a String.py
@@ -0,0 +1,44 @@
+Description:
+Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+ 
+
+Example 1:
+
+Input: s = "leetcode"
+
+Output: 0
+
+Explanation:
+
+The character 'l' at index 0 is the first character that does not occur at any other index.
+
+Example 2:
+
+Input: s = "loveleetcode"
+
+Output: 2
+
+Example 3:
+
+Input: s = "aabb"
+
+Output: -1
+
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of only lowercase English letters.
+
+Python3:
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        count = Counter(s)
+        for i, ch in enumerate(s):
+            if count[ch] == 1:
+                return i 
+        return -1
+
+       
